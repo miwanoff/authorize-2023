f63d796125c6d65bc7ac44b3de9625f2ce57026e
miwanoff <marina.iva.noffa@gmail.com>
2024-12-21 12:36:49 +0200

    $_SESSION['login'] was moved

--- authorize.php
+++ authorize.php
@@ -1,27 +1,28 @@
 <?php
-session_start ();
+session_start();
 // створюємо нову сесію або відновлюємо поточну
 if (!isset($_GET['go'])) {
-echo "<form>
+    echo "<form>
     Login: <input type='text' name='login'>
     Password: <input type='password' name='passwd'>
     <input type='submit' name='go' value='Go'>
 </form>";
-if (isset($_SESSION ['err']) && ($_SESSION ['err'] == 1)) {
-$_SESSION ['err'] = 0;
-echo "Неправильне введення, спробуйте ще раз!<br>";
-}
+    if (isset($_SESSION['err']) && ($_SESSION['err'] == 1)) {
+        $_SESSION['err'] = 0;
+        echo "Неправильне введення, спробуйте ще раз!<br>";
+    }
 } else {
-$_SESSION ['login'] = $_GET ['login'];
-$_SESSION ['passwd'] = $_GET ['passwd'];
+   
 // реєструємо змінні login та passwd як глобальні змінні для цієї сесії
-if ($_GET ['login'] == "pit" && $_GET ['passwd'] == "123") {
-$_SESSION ['authorized'] = 1;
-header ( "Location: secret_info.php" );
+    if ($_GET['login'] == "pit" && $_GET['passwd'] == "123") {
+        $_SESSION['login'] = $_GET['login'];
+        $_SESSION['passwd'] = $_GET['passwd'];
+        $_SESSION['authorized'] = 1;
+        header("Location: secret_info.php");
 // перенаправляємо на сторінку secret_info.php
-} else {
-$_SESSION ['err'] = 1;
-unset($_SESSION['authorized']);
-header ( "Location: authorize.php" );
-}
+    } else {
+        $_SESSION['err'] = 1;
+        unset($_SESSION['authorized']);
+        header("Location: authorize.php");
+    }
 }
--- secret_info.php
+++ secret_info.php
@@ -1,8 +1,11 @@
 <?php
-session_start(); // створюємо нову сесію або відновлюємо поточну   
-if (!isset($_SESSION['authorized']))  // перевіряємо правильність авторизації    
-    header("Location: authorize.php"); 
-    // якщо помилка, то перенаправляємо на сторінку авторизації
+session_start(); // створюємо нову сесію або відновлюємо поточну
+if (!isset($_SESSION['authorized'])) // перевіряємо правильність авторизації
+{
+    header("Location: authorize.php");
+}
+
+// якщо помилка, то перенаправляємо на сторінку авторизації
 ?>
 <html>
 
@@ -10,8 +13,8 @@ if (!isset($_SESSION['authorized']))  // перевіряємо правильн
     <title>Secret info</title>
 </head>
 <!-- здесь располагается "секретна інформація"-->
-<?php 
-    print_r($_SESSION);// виводимо змінні сесії ?>
+<?php
+print_r($_SESSION); // виводимо змінні сесії ?>
 <br><a href="index.php">На главную</a>
 
 </html>
